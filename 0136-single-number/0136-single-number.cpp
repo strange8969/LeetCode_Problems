c4ca47836aa75e0d6ed2545c9e3530cd8cea9a40
Aaditya Raj <113026322+strange8969@users.noreply.github.com>
2024-08-11 11:45:02 +0530

    Time: 11 ms (82.22%), Space: 19.5 MB (47.78%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         int ans=0;
       for(int i=0;i<nums.size();i++){
-        ans = ans^nums[i];
+        ans^=nums[i];
       } 
       return ans; 
     }
