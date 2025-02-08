ac76a367126a432205f98c7dd30af98dfe0ec476
Aaditya Raj <113026322+strange8969@users.noreply.github.com>
2025-02-08 11:47:37 +0530

    Time: 0 ms (100%), Space: 17.4 MB (86.65%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,56 +1,47 @@
 class Solution {
 public:
+    int findLeftMost(vector <int> &nums, int target, int n){
+        int l = 0;
+        int r = n-1;
+        int left_most = -1;
 
-    int firstOccur(vector <int> &nums,int target){
-        int n = nums.size();
-        int first=-1;
-        int low=0;
-        int high=n-1;
-
-        while(low<=high){
-            int mid=(low+high)/2;
+        while(l<=r){
+            int mid = l+(r-l)/2;
             if(nums[mid]==target){
-                first=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
+                left_most = mid;
+                r=mid-1;
+            }else if(nums[mid] < target){
+                l = mid+1;
+            }else{
+                r=mid-1;
             }
         }
-        return first;
+        return left_most;
     }
-    int lastOccur(vector <int> &nums , int target){
-        int n=nums.size();
-        int last=-1;
-        int low=0;
-        int high=n-1;
 
-        while(low<=high){
-            int mid=(low+high)/2;
+    int findRightMost(vector <int> &nums, int target, int n){
+        int l = 0;
+        int r = n-1;
+        int right_most = -1;
+
+        while(l<=r){
+            int mid = l+(r-l)/2;
             if(nums[mid]==target){
-                last=mid;
-                low=mid+1;
+                right_most=mid;
+                l=mid+1;
+            }else if(nums[mid] < target){
+                l = mid+1;
+            }else{
+                r= mid-1;
             }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+        }
+        return right_most;
     }
-    return last;
-    }
-
 
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
-        int first =firstOccur(nums,target);
-        if(first==-1) return {-1,-1};
-        int last = lastOccur(nums,target);
-        return {first,last};
-        
+        int left_most = findLeftMost(nums,target,n);
+        int right_most = findRightMost(nums,target,n);
+        return {left_most,right_most};
     }
 };
