746e88c14f4025afdc9178c65052e4d33423919b
Aaditya Raj <adityaraj896919@gmail.com>
2025-05-27 02:22:01 +0530

    Time: 0 ms (100%), Space: 8.8 MB (25.46%) - LeetHub

--- 3560-find-minimum-log-transportation-cost/3560-find-minimum-log-transportation-cost.cpp
+++ 3560-find-minimum-log-transportation-cost/3560-find-minimum-log-transportation-cost.cpp
@@ -4,10 +4,10 @@ public:
         if(n<=k && m<=k) return 0;
         long long cost = 0;
         if(n>k){
-            cost += k*(n-k);
+            cost += 1LL*k*(n-k);
         }
         if(m>k){
-            cost += k*(m-k);
+            cost += 1LL*k*(m-k);
         }
         return cost;
     }
