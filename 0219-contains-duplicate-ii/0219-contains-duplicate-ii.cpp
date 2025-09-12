9631816a0c325008b345129c1ca66d17c7eb1670
Aaditya Raj <adityaraj896919@gmail.com>
2025-09-12 21:24:43 +0530

    Time: 74 ms (58.43%), Space: 98.7 MB (32.67%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map <int,int> lastSeen;
+
+        for(int i=0;i<nums.size();i++){
+            if(lastSeen.count(nums[i])){
+                if(i-lastSeen[nums[i]] <= k){
+                    return true;
+                }
+            }
+            lastSeen[nums[i]] = i;
+        }
+        return false;
+    }
+};
