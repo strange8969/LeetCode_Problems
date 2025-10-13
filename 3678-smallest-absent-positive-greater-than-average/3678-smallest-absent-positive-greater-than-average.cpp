7529f5458d7dd81b9f6299f3f30f2bbd3b0c9152
Aaditya Raj <adityaraj896919@gmail.com>
2025-10-14 04:18:44 +0530

    Time: 39 ms (5.02%), Space: 92.7 MB (41.96%) - LeetHub

---
+++ 3678-smallest-absent-positive-greater-than-average/3678-smallest-absent-positive-greater-than-average.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int smallestAbsent(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(int x:nums){
+            sum += x;
+        }
+        long long avg = sum / n;
+        avg++;
+        unordered_set<int> st(nums.begin(),nums.end());
+        if(avg <= 0) avg=1;
+        while(true){
+            if(st.count(avg) == 0) return avg;
+            avg++;
+        }
+        return -1;
+    }
+};
