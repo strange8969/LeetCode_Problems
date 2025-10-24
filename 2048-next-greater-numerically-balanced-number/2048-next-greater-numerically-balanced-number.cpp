f254115a1a6cde5a4edc9f39711b756a1d69a567
Aaditya Raj <adityaraj896919@gmail.com>
2025-10-25 00:06:19 +0530

    Time: 551 ms (24.62%), Space: 254.8 MB (27.14%) - LeetHub

---
+++ 2048-next-greater-numerically-balanced-number/2048-next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool balanced(int num){
+        vector<int> freq(10);
+        while(num > 0){
+            int digit = num%10;
+            freq[digit]++;
+            num/=10;
+        }
+        for(int d=0;d<10;d++){
+            if(freq[d] != 0 && freq[d]!=d){
+                return false;
+            }
+        }
+        return true;
+    }
+    int nextBeautifulNumber(int n) {
+        for(int num=n+1;num<=1224444;num++){
+            if(balanced(num)){
+                return num;
+            }
+        }
+        return -1;
+    }
+};
